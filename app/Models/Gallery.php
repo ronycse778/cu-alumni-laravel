3d4be4a1de20ccbef8f7d138d3fa6ff631ecffb1
ronycse778 <80318633+ronycse778@users.noreply.github.com>
2025-10-18 16:49:04 +0600

    Add app/Models/Gallery.php

---
+++ app/Models/Gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'order',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}

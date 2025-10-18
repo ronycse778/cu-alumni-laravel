93d8c75bc7771498d44fccc1615e66162e6a1ff6
ronycse778 <80318633+ronycse778@users.noreply.github.com>
2025-10-18 16:49:04 +0600

    Add app/Models/News.php

---
+++ app/Models/News.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'content',
+        'image',
+        'published_date',
+        'order',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'published_date' => 'date',
+    ];
+}
